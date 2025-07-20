36723df56342bc238eafb7ee4deff1e2e6d5c72d
GoldenVadim <78044586+GoldenVadim@users.noreply.github.com>
2025-07-20 06:00:40 +0300

    Update InstallKernel&Firmware.sh

--- InstallKernel&Firmware.sh
+++ InstallKernel&Firmware.sh
@@ -12,29 +12,29 @@ sudo mkdir -p $BRCM
 
 # Kernel
 git clone --depth=1 https://github.com/raspberrypi/firmware.git
-sudo cp ${WORK}/firmware/boot/bcm2712-rpi-5-b.dtb ${DEST}/boot/
-sudo cp ${WORK}/firmware/boot/fixup_cd.dat ${DEST}/boot/
-sudo cp ${WORK}/firmware/boot/fixup.dat ${DEST}/boot/
-sudo cp ${WORK}/firmware/boot/start_cd.elf ${DEST}/boot/
-sudo cp ${WORK}/firmware/boot/start.elf ${DEST}/boot/
-sudo cp ${WORK}/firmware/boot/bootcode.bin ${DEST}/boot/
-sudo cp ${WORK}/firmware/boot/kernel_2712.img ${DEST}/boot/
-sudo cp -r ${WORK}/firmware/boot/overlays ${DEST}/boot/
-sudo cp -r ${WORK}/firmware/modules ${DEST}/lib/
+sudo cp -v ${WORK}/firmware/boot/bcm2712-rpi-5-b.dtb ${DEST}/boot/
+sudo cp -v ${WORK}/firmware/boot/fixup_cd.dat ${DEST}/boot/
+sudo cp -v ${WORK}/firmware/boot/fixup.dat ${DEST}/boot/
+sudo cp -v ${WORK}/firmware/boot/start_cd.elf ${DEST}/boot/
+sudo cp -v ${WORK}/firmware/boot/start.elf ${DEST}/boot/
+sudo cp -v ${WORK}/firmware/boot/bootcode.bin ${DEST}/boot/
+sudo cp -v ${WORK}/firmware/boot/kernel_2712.img ${DEST}/boot/
+sudo cp -rv ${WORK}/firmware/boot/overlays ${DEST}/boot/
+sudo cp -rv ${WORK}/firmware/modules ${DEST}/lib/
 
 # Wi-Fi
 git clone --depth=1 https://github.com/RPi-Distro/firmware-nonfree.git
-sudo cp ${WORK}/firmware-nonfree/debian/config/brcm80211/cypress/cyfmac43455-sdio-standard.bin ${DEST}/lib/firmware/brcm/
-sudo cp ${WORK}/firmware-nonfree/debian/config/brcm80211/cypress/cyfmac43455-sdio.clm_blob ${DEST}/lib/firmware/brcm/
-sudo cp ${WORK}/firmware-nonfree/debian/config/brcm80211/brcm/brcmfmac43455-sdio.txt ${DEST}/lib/firmware/brcm/
-sudo ln -s cyfmac43455-sdio-standard.bin $BRCM/brcmfmac43455-sdio.raspberrypi,5-model-b.bin
-sudo ln -s cyfmac43455-sdio.clm_blob $BRCM/brcmfmac43455-sdio.raspberrypi,5-model-b.clm_blob
-sudo ln -s brcmfmac43455-sdio.txt $BRCM/brcmfmac43455-sdio.raspberrypi,5-model-b.txt
+sudo cp -v ${WORK}/firmware-nonfree/debian/config/brcm80211/cypress/cyfmac43455-sdio-standard.bin ${DEST}/lib/firmware/brcm/
+sudo cp -v ${WORK}/firmware-nonfree/debian/config/brcm80211/cypress/cyfmac43455-sdio.clm_blob ${DEST}/lib/firmware/brcm/
+sudo cp -v ${WORK}/firmware-nonfree/debian/config/brcm80211/brcm/brcmfmac43455-sdio.txt ${DEST}/lib/firmware/brcm/
+sudo ln -sv cyfmac43455-sdio-standard.bin $BRCM/brcmfmac43455-sdio.raspberrypi,5-model-b.bin
+sudo ln -sv cyfmac43455-sdio.clm_blob $BRCM/brcmfmac43455-sdio.raspberrypi,5-model-b.clm_blob
+sudo ln -sv brcmfmac43455-sdio.txt $BRCM/brcmfmac43455-sdio.raspberrypi,5-model-b.txt
 
 # Bluetooth
 git clone --depth=1 https://github.com/RPi-Distro/bluez-firmware.git
-sudo cp ${WORK}/bluez-firmware/debian/firmware/broadcom/BCM4345C0.hcd ${DEST}/lib/firmware/brcm/
-sudo ln -s BCM4345C0.hcd $BRCM/BCM4345C0.raspberrypi,5-model-b.hcd
+sudo cp -v ${WORK}/bluez-firmware/debian/firmware/broadcom/BCM4345C0.hcd ${DEST}/lib/firmware/brcm/
+sudo ln -sv BCM4345C0.hcd $BRCM/BCM4345C0.raspberrypi,5-model-b.hcd
 
 
 ls -l $BRCM
