d940231233f054ff70ae5f00685aa13cf49e6bf6
GoldenVadim <sujumai002@gmail.com>
2025-07-20 06:17:22 +0300

    fixed mounting

--- Mount.sh
+++ Mount.sh
@@ -3,9 +3,8 @@
 DISK=/dev/sda # /mnt/mmcblk0 for RPi 5
 DEST=Root
 
-sudo umount $DISK*
-mkdir -p $DEST/boot
-sudo mount /dev/sda2 $DEST
-sudo mount /dev/sda1 $DEST/boot
+sudo umount $DISK
+sudo mount /dev/sda2 $DEST -m
+sudo mount /dev/sda1 $DEST/boot -m
 
 lsblk
