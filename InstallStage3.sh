03bbde6005471d7dd08eeb83bb0ee231c50f8867
GoldenVadim <78044586+GoldenVadim@users.noreply.github.com>
2025-08-15 09:30:53 +0300

    Update InstallStage3.sh

--- InstallStage3.sh
+++ InstallStage3.sh
@@ -7,13 +7,13 @@ file stage3-arm64*
 sudo tar xvpf stage3-arm64*.tar.xz --numeric-owner --xattrs-include="*.*" -C $DEST
 
 echo "Run these commands if you are installed with systemd profile:"
-echo "systemd-firstboot --root=${DEST} --reset --prompt"
-echo "systemd-machine-id-setup --root=${DEST}"
-echo "systemctl --root=${DEST} preset-all"
-echo "systemctl --root=${DEST} preset-all --global"
+echo " systemd-firstboot --root=${DEST} --reset --prompt"
+echo " systemd-machine-id-setup --root=${DEST}"
+echo " systemctl --root=${DEST} preset-all"
+echo " systemctl --root=${DEST} preset-all --global"
 
 echo "Password of root will be 'raspberry'."
 sed -i "1c$(cat shadow)" $DEST/etc/shadow
 
 echo "Comment this string in ${DEST}/etc/inittab"
-echo 'f0:12345:respawn:/sbin/agetty 9600 ttyAMA0 vt100'
+echo ' f0:12345:respawn:/sbin/agetty 9600 ttyAMA0 vt100'
