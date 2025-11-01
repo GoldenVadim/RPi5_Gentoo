ae3d481a0a594bb07f2a0a1bbf29042e1325004a
GoldenVadim <78044586+GoldenVadim@users.noreply.github.com>
2025-11-01 22:32:20 +0200

    Update InstallFirmwareBoot.sh

--- InstallFirmwareBoot.sh
+++ InstallFirmwareBoot.sh
@@ -5,15 +5,15 @@ cp -r $WORK/firmware/boot/overlays $DEST/boot/
 cp -r $WORK/firmware/modules/*-v8-16k+ $DEST/lib/
 cp $WORK/firmware/extra/System_2712.map $DEST/boot
 
+echo ' * Copying Wi-Fi & Bluetooth firmware...'
 mkdir -p $BRCM
-
-echo ' * Copying firmware...'
+# Wi-Fi
 cp $WORK/firmware-nonfree/debian/config/brcm80211/cypress/cyfmac43455-sdio-standard.bin $BRCM
 cp $WORK/firmware-nonfree/debian/config/brcm80211/cypress/cyfmac43455-sdio.clm_blob $BRCM
 cp $WORK/firmware-nonfree/debian/config/brcm80211/brcm/brcmfmac43455-sdio.txt $BRCM
 ln -s cyfmac43455-sdio-standard.bin $BRCM/brcmfmac43455-sdio.raspberrypi,5-model-b.bin
 ln -s cyfmac43455-sdio.clm_blob $BRCM/brcmfmac43455-sdio.raspberrypi,5-model-b.clm_blob
 ln -s brcmfmac43455-sdio.txt $BRCM/brcmfmac43455-sdio.raspberrypi,5-model-b.txt
-
+# Bluetooth
 cp $WORK/bluez-firmware/debian/firmware/broadcom/BCM4345C0.hcd $BRCM
 ln -s BCM4345C0.hcd $BRCM/BCM4345C0.raspberrypi,5-model-b.hcd
